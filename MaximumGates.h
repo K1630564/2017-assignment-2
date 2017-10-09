edcd1e140821b3e3d14b0c1c0a0853603829cf28
martinkoling <martin.koling@kcl.ac.uk>
2017-10-09 23:56:09 +0100

    Completed first task

--- MaximumGates.h
+++ MaximumGates.h
@@ -1,11 +1,52 @@
+
 #ifndef MAXIMUM_GATES_H
 #define MAXIMUM_GATES_H
 
 #include <vector>
+#include "vector.h"
+#include <iostream>
+#include <iterator>
+#include <algorithm>
 using std::vector;
+using std::cout;
+using std::min_element;
+using std::max_element;
+using std::distance;
 
-// TODO: Your answer for question 2 goes here
 
+
+
+    int maximumGates(vector<int> arrives, vector<int> departs) {
+
+        int gatesInUse = 0;
+        int maxGates = 0;
+        int departSmallestIndex = distance(departs.begin(), min_element(departs.begin(), departs.end()));
+        int departSmallestValue = departs[departSmallestIndex];
+
+        for (int i = 0; i < arrives.size(); i++) {
+
+            if(arrives[i] < departSmallestValue){
+                gatesInUse++;
+            }
+
+            if(arrives[i] > departSmallestValue){
+                gatesInUse --;
+
+
+            }
+
+            if(gatesInUse >= maxGates){
+
+                maxGates = gatesInUse;
+            }
+
+
+
+        }
+
+
+        return maxGates;
+    }
 
 
 
